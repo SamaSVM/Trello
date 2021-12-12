aac214742cb627febca7683bb874979440200bfa
samas <sama.svm@gmail.com>
2021-12-12 16:10:04 +0200

    added card and board services

---
+++ src/main/java/spd/trello/services/BoardService.java
@@ -0,0 +1,10 @@
+package spd.trello.services;
+
+import spd.trello.domain.Board;
+
+public class BoardService extends AbstractService<Board> {
+    @Override
+    public Board create() {
+        return null;
+    }
+}
---
+++ src/main/java/spd/trello/services/CardService.java
@@ -0,0 +1,10 @@
+package spd.trello.services;
+
+import spd.trello.domain.Card;
+
+public class CardService extends AbstractService<Card>{
+    @Override
+    public Card create() {
+        return null;
+    }
+}
