8bc8657b02361fa227c5b286f05bef014932ba99
samas <sama.svm@gmail.com>
2021-12-12 14:55:44 +0200

    removed creationDate

--- src/main/java/spd/trello/domain/Card.java
+++ src/main/java/spd/trello/domain/Card.java
@@ -2,7 +2,6 @@ package spd.trello.domain;
 
 import lombok.Data;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,5 +16,4 @@ public class Card {
     private List<Comment> comments;
     private Reminder reminder;
     private List<Checklist> checklists;
-    private LocalDateTime creationDate;
 }
