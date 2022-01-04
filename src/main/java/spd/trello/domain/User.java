8b5920625f69380c02ebe966de2b888f4b49ea4a
samas <sama.svm@gmail.com>
2022-01-04 17:51:56 +0200

    added all levels for the user

--- src/main/java/spd/trello/domain/User.java
+++ src/main/java/spd/trello/domain/User.java
@@ -3,14 +3,13 @@ package spd.trello.domain;
 import lombok.Data;
 import spd.trello.domain.perent.Domain;
 
-import java.util.TimeZone;
 
 @Data
 public class User extends Domain {
     private String firstName;
     private String lastName;
     private String email;
-    private TimeZone timeZone;
+    private String timeZone;
 
     @Override
     public String toString() {
---
+++ src/main/java/spd/trello/repository/UserRepository.java
@@ -0,0 +1,110 @@
+package spd.trello.repository;
+
+;
+import spd.trello.domain.User;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.UUID;
+
+public class UserRepository implements InterfaceRepository<User> {
+    private final DataSource dataSource;
+    private final String CREATE_STMT =
+            "INSERT INTO users (id, first_name, last_name, email, time_zone) VALUES (?, ?, ?, ?, ?);";
+    private final String FIND_BY_ID_STMT = "SELECT * FROM users WHERE id=?;";
+    private final String DELETE_BY_ID_STMT = "DELETE FROM users WHERE id=?;";
+    private final String UPDATE_BY_ENTITY_STMT =
+            "UPDATE users SET first_name=?, last_name=?, email=?, time_zone=? WHERE id=?;";
+
+    public UserRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public User findById(UUID id) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_STMT)) {
+            statement.setObject(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return map(resultSet);
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("UserRepository::findUserById failed", e);
+        }
+        throw new IllegalStateException("User with ID: " + id.toString() + " doesn't exists");
+    }
+
+    @Override
+    public void create(User entity) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(CREATE_STMT)) {
+            statement.setObject(1, entity.getId());
+            statement.setString(2, entity.getFirstName());
+            statement.setString(3, entity.getLastName());
+            statement.setString(4, entity.getEmail());
+            statement.setString(5, entity.getTimeZone());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException("User doesn't creates");
+        }
+    }
+
+    @Override
+    public User update(User entity) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(UPDATE_BY_ENTITY_STMT)) {
+            User oldUser = findById(entity.getId());
+            if (entity.getFirstName() == null) {
+                statement.setString(1, oldUser.getFirstName());
+            } else {
+                statement.setString(1, entity.getFirstName());
+            }
+
+            if (entity.getLastName() == null) {
+                statement.setString(2, oldUser.getLastName());
+            } else {
+                statement.setString(2, entity.getLastName());
+            }
+
+            if (entity.getEmail() == null) {
+                statement.setString(3, oldUser.getEmail());
+            } else {
+                statement.setString(3, entity.getEmail());
+            }
+
+            if (entity.getTimeZone() == null) {
+                statement.setString(4, oldUser.getTimeZone());
+            } else {
+                statement.setString(4, entity.getTimeZone());
+            }
+
+            statement.setObject(5, entity.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new IllegalStateException("User with ID: " + entity.getId().toString() + " doesn't updates");
+        }
+        return findById(entity.getId());
+    }
+
+    @Override
+    public boolean delete(UUID id) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID_STMT)) {
+            statement.setObject(1, id);
+            return statement.executeUpdate() == 1;
+        } catch (SQLException e) {
+            throw new IllegalStateException("UserRepository::delete failed", e);
+        }
+    }
+
+    private User map(ResultSet rs) throws SQLException {
+        User user = new User();
+        user.setId(UUID.fromString(rs.getString("id")));
+        user.setFirstName(rs.getString("first_name"));
+        user.setLastName(rs.getString("last_name"));
+        user.setEmail(rs.getString("email"));
+        user.setTimeZone(rs.getString("time_zone"));
+        return user;
+    }
+}
