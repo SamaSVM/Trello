6ff45b6224e0dd2c7e7e71c4ef2891cca30b5348
samas <sama.svm@gmail.com>
2022-01-08 15:12:33 +0200

    added unique values for many-to-many tables

--- src/main/resources/db/migration/V4__create_member_workspace_table.sql
+++ src/main/resources/db/migration/V4__create_member_workspace_table.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS member_workspace
     member_id    UUID NOT NULL,
     workspace_id UUID NOT NULL,
     FOREIGN KEY (member_id) REFERENCES members (id),
-    FOREIGN KEY (workspace_id) REFERENCES workspaces (id)
+    FOREIGN KEY (workspace_id) REFERENCES workspaces (id),
+    UNIQUE (member_id, workspace_id)
 );
