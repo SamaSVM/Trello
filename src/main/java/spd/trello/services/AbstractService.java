bab044848b165dfde2aa03b297904495d8981d47
samas <sama.svm@gmail.com>
2022-01-13 20:11:58 +0200

    refactored AbstractService

--- src/main/java/spd/trello/services/AbstractService.java
+++ src/main/java/spd/trello/services/AbstractService.java
@@ -1,12 +1,47 @@
 package spd.trello.services;
 
+import com.zaxxer.hikari.HikariDataSource;
+import spd.trello.ConnectionPool;
+import spd.trello.domain.Member;
+import spd.trello.domain.enums.MemberRole;
 import spd.trello.domain.perent.Domain;
 import spd.trello.repository.InterfaceRepository;
+
+import java.util.List;
+import java.util.UUID;
 
 public abstract class AbstractService<T extends Domain> {
     protected InterfaceRepository<T> repository;
 
     public AbstractService(InterfaceRepository<T> repository) {
         this.repository = repository;
+    }
+protected HikariDataSource dataSource;
+    public AbstractService() {
+        this.dataSource = (HikariDataSource) ConnectionPool.createDataSource().getDataSource();
+    }
+
+    public T findById(UUID id) {
+        return repository.findById(id);
+    }
+
+    public List<T> findAll() {
+        return repository.findAll();
+    }
+
+    public T create(T entity) {
+        repository.create(entity);
+        return repository.findById(entity.getId());
+    }
+
+    public T update(Member member, T entity) {
+        if (member.getMemberRole() == MemberRole.GUEST) {
+            throw new IllegalStateException("This user cannot update!");
+        }
+        return repository.update(entity);
+    }
+
+    public boolean delete(UUID id) {
+        return repository.delete(id);
     }
 }
