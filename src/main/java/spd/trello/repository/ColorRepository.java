67df3177d1a4167542818577d240529431ef3a26
samas <sama.svm@gmail.com>
2022-01-27 20:18:37 +0200

    added jdbcTemplate for color

--- src/main/java/spd/trello/repository/ColorRepository.java
+++ src/main/java/spd/trello/repository/ColorRepository.java
@@ -1,24 +1,19 @@
 package spd.trello.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import spd.trello.domain.Color;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class ColorRepository implements InterfaceRepository<Color>{
-    public ColorRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    private final DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private final String CREATE_STMT =
             "INSERT INTO colors (id, red, green, blue, label_id) VALUES (?, ?, ?, ?, ?);";
@@ -33,62 +28,45 @@ public class ColorRepository implements InterfaceRepository<Color>{
 
     @Override
     public Color findById(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return map(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("CommentRepository::findCardById failed", e);
-        }
-        throw new IllegalStateException("Color with ID: " + id.toString() + " doesn't exists");
+        return jdbcTemplate.query(FIND_BY_ID_STMT, new Object[]{id}, new BeanPropertyRowMapper<>(Color.class))
+                .stream().findAny()
+                .orElseThrow(() -> new IllegalStateException("Color with ID: " + id.toString() + " doesn't exists"));
     }
 
     @Override
     public List<Color> findAll() {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL_STMT)) {
-            List<Color> result = new ArrayList<>();
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                result.add(map(resultSet));
-            }
-            if (!result.isEmpty()) {
-                return result;
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("ColorRepository::findAll failed", e);
+        List<Color> result = jdbcTemplate.query(FIND_ALL_STMT, new BeanPropertyRowMapper<>(Color.class));
+        if (result.isEmpty()) {
+            throw new IllegalStateException("Table colors is empty!");
         }
-        throw new IllegalStateException("Table colors is empty!");
+        return result;
     }
 
     @Override
     public void create(Color entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_STMT)) {
-            statement.setObject(1, entity.getId());
-            statement.setInt(2, entity.getRed());
-            statement.setInt(3, entity.getGreen());
-            statement.setInt(4, entity.getBlue());
-            statement.setObject(5, entity.getLabelId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    CREATE_STMT,
+                    entity.getId(),
+                    entity.getRed(),
+                    entity.getGreen(),
+                    entity.getBlue(),
+                    entity.getLabelId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("Color doesn't creates");
         }
     }
 
     @Override
     public Color update(Color entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_BY_ENTITY_STMT)) {
-            statement.setInt(1, entity.getRed());
-            statement.setInt(2, entity.getGreen());
-            statement.setInt(3, entity.getBlue());
-            statement.setObject(4, entity.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    UPDATE_BY_ENTITY_STMT,
+                    entity.getRed(),
+                    entity.getGreen(),
+                    entity.getBlue(),
+                    entity.getId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("Color with ID: " + entity.getId().toString() + " doesn't updates");
         }
         return findById(entity.getId());
@@ -96,22 +74,10 @@ public class ColorRepository implements InterfaceRepository<Color>{
 
     @Override
     public boolean delete(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
+        try {
+            return jdbcTemplate.update(DELETE_BY_ID_STMT, id) == 1;
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("ColorRepository::delete failed", e);
         }
-    }
-
-    private Color map(ResultSet rs) throws SQLException {
-        Color color = new Color();
-        color.setId(UUID.fromString(rs.getString("id")));
-        color.setRed(rs.getInt("red"));
-        color.setGreen(rs.getInt("green"));
-        color.setBlue(rs.getInt("blue"));
-        color.setLabelId(UUID.fromString(rs.getString("label_id")));
-        return color;
     }
 }
