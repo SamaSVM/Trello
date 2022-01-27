10ac379d23cf1eac87a741e4f705ac0ed187bfd1
samas <sama.svm@gmail.com>
2022-01-27 20:09:09 +0200

    added jdbcTemplate for card

--- src/main/java/spd/trello/repository/CardRepository.java
+++ src/main/java/spd/trello/repository/CardRepository.java
@@ -1,24 +1,19 @@
 package spd.trello.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import spd.trello.domain.*;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class CardRepository implements InterfaceRepository<Card> {
-    public CardRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    private final DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private final String CREATE_STMT =
             "INSERT INTO cards (id, created_by, created_date, name, description, archived, card_list_id)" +
@@ -35,66 +30,49 @@ public class CardRepository implements InterfaceRepository<Card> {
 
     @Override
     public Card findById(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return map(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("CardRepository::findCardById failed", e);
-        }
-        throw new IllegalStateException("Card with ID: " + id.toString() + " doesn't exists");
+        return jdbcTemplate.query(FIND_BY_ID_STMT, new Object[]{id}, new BeanPropertyRowMapper<>(Card.class))
+                .stream().findAny()
+                .orElseThrow(() -> new IllegalStateException("Card with ID: " + id.toString() + " doesn't exists"));
     }
 
     @Override
     public List<Card> findAll() {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL_STMT)) {
-            List<Card> result = new ArrayList<>();
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                result.add(map(resultSet));
-            }
-            if (!result.isEmpty()) {
-                return result;
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("CardRepository::findAll failed", e);
+        List<Card> result = jdbcTemplate.query(FIND_ALL_STMT, new BeanPropertyRowMapper<>(Card.class));
+        if(result.isEmpty()){
+            throw new IllegalStateException("Table cards is empty!");
         }
-        throw new IllegalStateException("Table cards is empty!");
+        return result;
     }
 
     @Override
     public void create(Card entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_STMT)) {
-            statement.setObject(1, entity.getId());
-            statement.setString(2, entity.getCreatedBy());
-            statement.setDate(3, entity.getCreatedDate());
-            statement.setString(4, entity.getName());
-            statement.setString(5, entity.getDescription());
-            statement.setBoolean(6, entity.getArchived());
-            statement.setObject(7, entity.getCardListId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    CREATE_STMT,
+                    entity.getId(),
+                    entity.getCreatedBy(),
+                    entity.getCreatedDate(),
+                    entity.getName(),
+                    entity.getDescription(),
+                    entity.getArchived(),
+                    entity.getCardListId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("Card doesn't creates");
         }
     }
 
     @Override
     public Card update(Card entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_BY_ENTITY_STMT)) {
-            statement.setString(1, entity.getUpdatedBy());
-            statement.setDate(2, entity.getUpdatedDate());
-            statement.setString(3, entity.getName());
-            statement.setString(4, entity.getDescription());
-            statement.setBoolean(5, entity.getArchived());
-            statement.setObject(6, entity.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    UPDATE_BY_ENTITY_STMT,
+                    entity.getUpdatedBy(),
+                    entity.getUpdatedDate(),
+                    entity.getName(),
+                    entity.getDescription(),
+                    entity.getArchived(),
+                    entity.getId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("Card with ID: " + entity.getId().toString() + " doesn't updates");
         }
         return findById(entity.getId());
@@ -102,26 +80,10 @@ public class CardRepository implements InterfaceRepository<Card> {
 
     @Override
     public boolean delete(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
+        try {
+            return jdbcTemplate.update(DELETE_BY_ID_STMT, id) == 1;
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("CardRepository::delete failed", e);
         }
-    }
-
-    private Card map(ResultSet rs) throws SQLException {
-        Card card = new Card();
-        card.setId(UUID.fromString(rs.getString("id")));
-        card.setCreatedBy(rs.getString("created_by"));
-        card.setUpdatedBy(rs.getString("updated_by"));
-        card.setCreatedDate(rs.getDate("created_date"));
-        card.setUpdatedDate(rs.getDate("updated_date"));
-        card.setName(rs.getString("name"));
-        card.setDescription(rs.getString("description"));
-        card.setArchived(rs.getBoolean("archived"));
-        card.setCardListId(UUID.fromString(rs.getString("card_list_id")));
-        return card;
     }
 }
