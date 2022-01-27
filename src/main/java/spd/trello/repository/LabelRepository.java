86bb14bc9631d432343d54b724f39c1a2f44793a
samas <sama.svm@gmail.com>
2022-01-27 20:13:57 +0200

    added jdbcTemplate for label

--- src/main/java/spd/trello/repository/LabelRepository.java
+++ src/main/java/spd/trello/repository/LabelRepository.java
@@ -1,24 +1,19 @@
 package spd.trello.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import spd.trello.domain.Label;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
-public class LabelRepository implements InterfaceRepository<Label>{
-    public LabelRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    private final DataSource dataSource;
+public class LabelRepository implements InterfaceRepository<Label> {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private final String CREATE_STMT =
             "INSERT INTO labels (id, name, card_id) VALUES (?, ?, ?);";
@@ -33,58 +28,41 @@ public class LabelRepository implements InterfaceRepository<Label>{
 
     @Override
     public Label findById(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return map(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("LabelRepository::findById failed", e);
-        }
-        throw new IllegalStateException("Label with ID: " + id.toString() + " doesn't exists");
+        return jdbcTemplate.query(FIND_BY_ID_STMT, new Object[]{id}, new BeanPropertyRowMapper<>(Label.class))
+                .stream().findAny()
+                .orElseThrow(() -> new IllegalStateException("Label with ID: " + id.toString() + " doesn't exists"));
     }
 
     @Override
     public List<Label> findAll() {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL_STMT)) {
-            List<Label> result = new ArrayList<>();
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                result.add(map(resultSet));
-            }
-            if (!result.isEmpty()) {
-                return result;
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("LabelRepository::findAll failed", e);
+        List<Label> result = jdbcTemplate.query(FIND_ALL_STMT, new BeanPropertyRowMapper<>(Label.class));
+        if (result.isEmpty()) {
+            throw new IllegalStateException("Table labels is empty!");
         }
-        throw new IllegalStateException("Table labels is empty!");
+        return result;
     }
 
     @Override
     public void create(Label entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_STMT)) {
-            statement.setObject(1, entity.getId());
-            statement.setString(2, entity.getName());
-            statement.setObject(3, entity.getCardId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    CREATE_STMT,
+                    entity.getId(),
+                    entity.getName(),
+                    entity.getCardId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("Label doesn't creates");
         }
     }
 
     @Override
     public Label update(Label entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_BY_ENTITY_STMT)) {
-            statement.setString(1, entity.getName());
-            statement.setObject(2, entity.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    UPDATE_BY_ENTITY_STMT,
+                    entity.getName(),
+                    entity.getId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("Label with ID: " + entity.getId().toString() + " doesn't updates");
         }
         return findById(entity.getId());
@@ -92,20 +70,10 @@ public class LabelRepository implements InterfaceRepository<Label>{
 
     @Override
     public boolean delete(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
+        try {
+            return jdbcTemplate.update(DELETE_BY_ID_STMT, id) == 1;
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("LabelRepository::delete failed", e);
         }
-    }
-
-    private Label map(ResultSet rs) throws SQLException {
-        Label label = new Label();
-        label.setId(UUID.fromString(rs.getString("id")));
-        label.setName(rs.getString("name"));
-        label.setCardId(UUID.fromString(rs.getString("card_id")));
-        return label;
     }
 }
