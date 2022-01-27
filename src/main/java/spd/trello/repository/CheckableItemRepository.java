f47bb6c4b98eacc6687a112d62bdaad27518348f
samas <sama.svm@gmail.com>
2022-01-27 20:34:39 +0200

    added jdbcTemplate for checkableItem

--- src/main/java/spd/trello/repository/CheckableItemRepository.java
+++ src/main/java/spd/trello/repository/CheckableItemRepository.java
@@ -1,24 +1,19 @@
 package spd.trello.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import spd.trello.domain.CheckableItem;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 @Repository
 public class CheckableItemRepository implements InterfaceRepository<CheckableItem>{
-    public CheckableItemRepository(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    private final DataSource dataSource;
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
     private final String CREATE_STMT =
             "INSERT INTO checkable_items (id, name, checked, checklist_id) VALUES (?, ?, ?, ?);";
@@ -33,60 +28,43 @@ public class CheckableItemRepository implements InterfaceRepository<CheckableIte
 
     @Override
     public CheckableItem findById(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            ResultSet resultSet = statement.executeQuery();
-            if (resultSet.next()) {
-                return map(resultSet);
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("CheckableItemRepository::findById failed", e);
-        }
-        throw new IllegalStateException("CheckableItem with ID: " + id.toString() + " doesn't exists");
+        return jdbcTemplate.query(FIND_BY_ID_STMT, new Object[]{id}, new BeanPropertyRowMapper<>(CheckableItem.class))
+                .stream().findAny()
+                .orElseThrow(() -> new IllegalStateException("CheckableItem with ID: " + id.toString() + " doesn't exists"));
     }
 
     @Override
     public List<CheckableItem> findAll() {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(FIND_ALL_STMT)) {
-            List<CheckableItem> result = new ArrayList<>();
-            ResultSet resultSet = statement.executeQuery();
-            while (resultSet.next()) {
-                result.add(map(resultSet));
-            }
-            if (!result.isEmpty()) {
-                return result;
-            }
-        } catch (SQLException e) {
-            throw new IllegalStateException("CheckableItemRepository::findAll failed", e);
+        List<CheckableItem> result = jdbcTemplate.query(FIND_ALL_STMT, new BeanPropertyRowMapper<>(CheckableItem.class));
+        if (result.isEmpty()) {
+            throw new IllegalStateException("Table checkable_items is empty!");
         }
-        throw new IllegalStateException("Table checkable_items is empty!");
+        return result;
     }
 
     @Override
     public void create(CheckableItem entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(CREATE_STMT)) {
-            statement.setObject(1, entity.getId());
-            statement.setString(2, entity.getName());
-            statement.setBoolean(3, entity.getChecked());
-            statement.setObject(4, entity.getChecklistId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    CREATE_STMT,
+                    entity.getId(),
+                    entity.getName(),
+                    entity.getChecked(),
+                    entity.getChecklistId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("CheckableItem doesn't creates");
         }
     }
 
     @Override
     public CheckableItem update(CheckableItem entity) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(UPDATE_BY_ENTITY_STMT)) {
-            statement.setString(1, entity.getName());
-            statement.setBoolean(2, entity.getChecked());
-            statement.setObject(3, entity.getId());
-            statement.executeUpdate();
-        } catch (SQLException e) {
+        try {
+            jdbcTemplate.update(
+                    UPDATE_BY_ENTITY_STMT,
+                    entity.getName(),
+                    entity.getChecked(),
+                    entity.getId());
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("CheckableItem with ID: " + entity.getId().toString() + " doesn't updates");
         }
         return findById(entity.getId());
@@ -94,21 +72,10 @@ public class CheckableItemRepository implements InterfaceRepository<CheckableIte
 
     @Override
     public boolean delete(UUID id) {
-        try (Connection connection = dataSource.getConnection();
-             PreparedStatement statement = connection.prepareStatement(DELETE_BY_ID_STMT)) {
-            statement.setObject(1, id);
-            return statement.executeUpdate() == 1;
-        } catch (SQLException e) {
+        try {
+            return jdbcTemplate.update(DELETE_BY_ID_STMT, id) == 1;
+        } catch (DataIntegrityViolationException e) {
             throw new IllegalStateException("CheckableItemRepository::delete failed", e);
         }
-    }
-
-    private CheckableItem map(ResultSet rs) throws SQLException {
-        CheckableItem checkableItem = new CheckableItem();
-        checkableItem.setId(UUID.fromString(rs.getString("id")));
-        checkableItem.setName(rs.getString("name"));
-        checkableItem.setChecked(rs.getBoolean("checked"));
-        checkableItem.setChecklistId(UUID.fromString(rs.getString("checklist_id")));
-        return checkableItem;
     }
 }
