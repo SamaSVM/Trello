495d661a164f21215a705bb31a59203968da351c
samas <sama.svm@gmail.com>
2022-01-31 15:51:30 +0200

    added spring boot

---
+++ src/main/java/spd/trello/Trello.java
@@ -0,0 +1,11 @@
+package spd.trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trello {
+    public static void main(String[] args) {
+        SpringApplication.run(Trello.class, args);
+    }
+}
