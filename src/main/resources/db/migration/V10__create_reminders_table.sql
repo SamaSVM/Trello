a14923825543268ee5ead2fe1a1ab5e6352728d9
samas <sama.svm@gmail.com>
2022-02-16 10:39:04 +0200

    refactored migrations

---
+++ src/main/resources/db/migration/V10__create_reminders_table.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS reminders
+(
+    id           UUID PRIMARY KEY NOT NULL,
+    created_by   VARCHAR(100)     NOT NULL,
+    updated_by   VARCHAR(100),
+    created_date TIMESTAMP        NOT NULL,
+    updated_date TIMESTAMP,
+    start        TIMESTAMP,
+    "end"        TIMESTAMP,
+    remind_on    TIMESTAMP        NOT NULL,
+    active       BOOLEAN          NOT NULL,
+    card_id      UUID UNIQUE      NOT NULL,
+    FOREIGN KEY (card_id) REFERENCES cards (id)
+);
---
+++ src/main/resources/db/migration/V13__create_labels_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS labels
+(
+    id      UUID PRIMARY KEY NOT NULL,
+    name    VARCHAR(255)     NOT NULL,
+    card_id UUID             NOT NULL,
+    FOREIGN KEY (card_id) REFERENCES cards (id)
+);
