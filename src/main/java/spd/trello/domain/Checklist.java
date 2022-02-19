ddca114af326e63f31bf887a116830cd1579f5c1
samas <sama.svm@gmail.com>
2022-02-19 16:20:31 +0200

    added equals and hashCode for checklist

--- src/main/java/spd/trello/domain/Checklist.java
+++ src/main/java/spd/trello/domain/Checklist.java
@@ -8,6 +8,7 @@ import spd.trello.domain.perent.Resource;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Data
@@ -24,4 +25,17 @@ public class Checklist extends Resource {
     @OneToMany(fetch = FetchType.EAGER, mappedBy = "checklistId", cascade = CascadeType.ALL)
     @JsonIgnoreProperties("checklist")
     private List<CheckableItem> checkableItems = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Checklist checklist = (Checklist) o;
+        return Objects.equals(name, checklist.name) && Objects.equals(cardId, checklist.cardId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, cardId);
+    }
 }
