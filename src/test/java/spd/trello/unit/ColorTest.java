ae515a360c670597900c0d3683367c54230e9ce1
samas <sama.svm@gmail.com>
2022-03-22 20:19:27 +0200

    refactored the relationship between the label and the color

--- src/test/java/spd/trello/unit/ColorTest.java
+++ src/test/java/spd/trello/unit/ColorTest.java
@@ -18,21 +18,9 @@ public class ColorTest {
     @Autowired
     private ColorService service;
 
-    @Autowired
-    private UnitHelper helper;
-
     @Test
     public void create() {
-        User user = helper.getNewUser("create@ColorT");
-        Member member = helper.getNewMember(user);
-        Workspace workspace = helper.getNewWorkspace(member);
-        Board board = helper.getNewBoard(member, workspace.getId());
-        CardList cardList = helper.getNewCardList(member, board.getId());
-        Card card = helper.getNewCard(member, cardList.getId());
-        Label label = helper.getNewLabel(card.getId());
-
         Color color = new Color();
-        color.setLabelId(label.getId());
         color.setRed(1);
         color.setGreen(2);
         color.setBlue(3);
@@ -42,31 +30,19 @@ public class ColorTest {
         assertAll(
                 () -> assertEquals(1, testColor.getRed()),
                 () -> assertEquals(2, testColor.getGreen()),
-                () -> assertEquals(3, testColor.getBlue()),
-                () -> assertEquals(label.getId(), testColor.getLabelId())
+                () -> assertEquals(3, testColor.getBlue())
         );
     }
 
     @Test
     public void findAll() {
-        User user = helper.getNewUser("findAll@ColorT");
-        Member member = helper.getNewMember(user);
-        Workspace workspace = helper.getNewWorkspace(member);
-        Board board = helper.getNewBoard(member, workspace.getId());
-        CardList cardList = helper.getNewCardList(member, board.getId());
-        Card card = helper.getNewCard(member, cardList.getId());
-        Label firstLabel = helper.getNewLabel(card.getId());
-        Label secondLabel = helper.getNewLabel(card.getId());
-
         Color firstColor = new Color();
-        firstColor.setLabelId(firstLabel.getId());
         firstColor.setRed(1);
         firstColor.setGreen(2);
         firstColor.setBlue(3);
         Color testFirstColor = service.save(firstColor);
 
         Color secondColor = new Color();
-        secondColor.setLabelId(secondLabel.getId());
         secondColor.setRed(4);
         secondColor.setGreen(5);
         secondColor.setBlue(6);
@@ -83,16 +59,7 @@ public class ColorTest {
 
     @Test
     public void findById() {
-        User user = helper.getNewUser("findById@ColorT");
-        Member member = helper.getNewMember(user);
-        Workspace workspace = helper.getNewWorkspace(member);
-        Board board = helper.getNewBoard(member, workspace.getId());
-        CardList cardList = helper.getNewCardList(member, board.getId());
-        Card card = helper.getNewCard(member, cardList.getId());
-        Label label = helper.getNewLabel(card.getId());
-
         Color color = new Color();
-        color.setLabelId(label.getId());
         color.setRed(1);
         color.setGreen(2);
         color.setBlue(3);
@@ -104,38 +71,17 @@ public class ColorTest {
 
     @Test
     public void delete() {
-        User user = helper.getNewUser("delete@ColorT");
-        Member member = helper.getNewMember(user);
-        Workspace workspace = helper.getNewWorkspace(member);
-        Board board = helper.getNewBoard(member, workspace.getId());
-        CardList cardList = helper.getNewCardList(member, board.getId());
-        Card card = helper.getNewCard(member, cardList.getId());
-        Label label = helper.getNewLabel(card.getId());
-
-        Color color = new Color();
-        color.setLabelId(label.getId());
-        color.setRed(1);
-        color.setGreen(2);
-        color.setBlue(3);
-        Color testColor = service.save(color);
-
-        assertNotNull(testColor);
-        service.delete(testColor.getId());
-        assertFalse(service.getAll().contains(testColor));
+        BadRequestException ex = assertThrows(
+                BadRequestException.class,
+                () -> service.delete(new Color().getId()),
+                "no exception"
+        );
+        assertTrue(ex.getMessage().contains("This method cannot be called!"));
     }
 
     @Test
     public void update() {
-        User user = helper.getNewUser("update@ColorT");
-        Member member = helper.getNewMember(user);
-        Workspace workspace = helper.getNewWorkspace(member);
-        Board board = helper.getNewBoard(member, workspace.getId());
-        CardList cardList = helper.getNewCardList(member, board.getId());
-        Card card = helper.getNewCard(member, cardList.getId());
-        Label label = helper.getNewLabel(card.getId());
-
         Color updateColor = new Color();
-        updateColor.setLabelId(label.getId());
         updateColor.setRed(1);
         updateColor.setGreen(2);
         updateColor.setBlue(3);
@@ -155,16 +101,6 @@ public class ColorTest {
     }
 
     @Test
-    public void createFailure() {
-        BadRequestException ex = assertThrows(
-                BadRequestException.class,
-                () -> service.save(new Color()),
-                "no exception"
-        );
-        assertTrue(ex.getMessage().contains("not-null property references a null or transient value"));
-    }
-
-    @Test
     public void findByIdFailure() {
         ResourceNotFoundException ex = assertThrows(
                 ResourceNotFoundException.class,
@@ -172,16 +108,5 @@ public class ColorTest {
                 "no exception"
         );
         assertEquals("Resource not found Exception!", ex.getMessage());
-    }
-
-    @Test
-    public void deleteFailure() {
-        UUID id = UUID.randomUUID();
-        BadRequestException ex = assertThrows(
-                BadRequestException.class,
-                () -> service.delete(id),
-                "no exception"
-        );
-        assertEquals("No class spd.trello.domain.Color entity with id " + id + " exists!", ex.getMessage());
     }
 }
