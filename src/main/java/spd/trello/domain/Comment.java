81295fd9bfc0711bf53720c22d685a9443c10dc5
samas <sama.svm@gmail.com>
2022-03-22 20:57:56 +0200

    deleted the relationship between the comment and the attachment

--- src/main/java/spd/trello/domain/Comment.java
+++ src/main/java/spd/trello/domain/Comment.java
@@ -7,8 +7,6 @@ import org.hibernate.annotations.LazyCollectionOption;
 import spd.trello.domain.perent.Resource;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -21,13 +19,4 @@ public class Comment extends Resource {
 
     @Column(name = "card_id")
     private UUID cardId;
-
-    @ElementCollection
-    @LazyCollection(LazyCollectionOption.FALSE)
-    @CollectionTable(
-            name = "attachments",
-            joinColumns=@JoinColumn(name= "comment_id")
-    )
-    @Column(name = "id")
-    private Set<UUID> attachments = new HashSet<>();
 }
