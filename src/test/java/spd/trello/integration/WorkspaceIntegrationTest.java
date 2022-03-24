827edd4da907b669ebee5e86dbe150bde897c8dc
samas <sama.svm@gmail.com>
2022-03-24 14:50:02 +0200

    refactored WorkspaceIntegrationTest

--- src/test/java/spd/trello/integration/WorkspaceIntegrationTest.java
+++ src/test/java/spd/trello/integration/WorkspaceIntegrationTest.java
@@ -7,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
 import spd.trello.domain.Member;
-import spd.trello.domain.User;
 import spd.trello.domain.Workspace;
 import spd.trello.domain.enums.WorkspaceVisibility;
 
@@ -20,66 +19,75 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-public class WorkspaceIntegrationTest extends AbstractIntegrationTest<User>{
+public class WorkspaceIntegrationTest extends AbstractIntegrationTest<Workspace> {
     private final String URL_TEMPLATE = "/workspaces";
 
     @Autowired
     private IntegrationHelper helper;
 
     @Test
-    public void create() throws Exception {
-        Member firstMember = helper.getNewMember("1create@WIT");
-        Workspace firstWorkspace = new Workspace();
-        firstWorkspace.setCreatedBy(firstMember.getCreatedBy());
+    public void createWithoutArg() throws Exception {
+        Member member = helper.getNewMember("createWithoutArg@WIT");
+        Workspace workspace = new Workspace();
+        workspace.setCreatedBy(member.getCreatedBy());
         Set<UUID> membersId = new HashSet<>();
-        membersId.add(firstMember.getId());
-        firstWorkspace.setMembersId(membersId);
-        firstWorkspace.setName("name");
-        MvcResult firstMvcResult = super.create(URL_TEMPLATE, firstWorkspace);
-        Set<UUID> testFirstMembersId = helper.getIdsFromJson(getValue(firstMvcResult, "$.membersId").toString());
-
-        Member secondMember = helper.getNewMember("2create@WIT");
-        Workspace secondWorkspace = new Workspace();
-        secondWorkspace.setCreatedBy(firstMember.getCreatedBy());
-        membersId.add(secondMember.getId());
-        secondWorkspace.setMembersId(membersId);
-        secondWorkspace.setName("name");
-        secondWorkspace.setDescription("description");
-        secondWorkspace.setVisibility(WorkspaceVisibility.PUBLIC);
-        MvcResult secondMvcResult = super.create(URL_TEMPLATE, secondWorkspace);
-        Set<UUID> testSecondMembersId = helper.getIdsFromJson(getValue(secondMvcResult, "$.membersId").toString());
+        membersId.add(member.getId());
+        workspace.setMembersId(membersId);
+        workspace.setName("name");
+        MvcResult mvcResult = super.create(URL_TEMPLATE, workspace);
+        Set<UUID> testMembersId = helper.getIdsFromJson(getValue(mvcResult, "$.membersId").toString());
 
         assertAll(
-                () -> assertEquals(HttpStatus.CREATED.value(), firstMvcResult.getResponse().getStatus()),
-                () -> assertNotNull(getValue(firstMvcResult, "$.id")),
-                () -> assertEquals(firstWorkspace.getCreatedBy(), getValue(firstMvcResult, "$.createdBy")),
-                () -> assertEquals(String.valueOf(LocalDate.now()), getValue(firstMvcResult, "$.createdDate")),
-                () -> assertNull(getValue(firstMvcResult, "$.updatedBy")),
-                () -> assertNull(getValue(firstMvcResult, "$.updatedDate")),
-                () -> assertEquals(firstWorkspace.getName(), getValue(firstMvcResult, "$.name")),
-                () -> assertNull(getValue(firstMvcResult, "$.description")),
-                () -> assertEquals(WorkspaceVisibility.PRIVATE.toString(), getValue(firstMvcResult, "$.visibility")),
-                () -> assertTrue(testFirstMembersId.contains(firstMember.getId())),
-                () -> assertEquals(1, testFirstMembersId.size()),
+                () -> assertEquals(HttpStatus.CREATED.value(), mvcResult.getResponse().getStatus()),
+                () -> assertNotNull(getValue(mvcResult, "$.id")),
+                () -> assertEquals(workspace.getCreatedBy(), getValue(mvcResult, "$.createdBy")),
+                () -> assertEquals(String.valueOf(LocalDate.now()), getValue(mvcResult, "$.createdDate")),
+                () -> assertNull(getValue(mvcResult, "$.updatedBy")),
+                () -> assertNull(getValue(mvcResult, "$.updatedDate")),
+                () -> assertEquals(workspace.getName(), getValue(mvcResult, "$.name")),
+                () -> assertNull(getValue(mvcResult, "$.description")),
+                () -> assertEquals(WorkspaceVisibility.PRIVATE.toString(),
+                        getValue(mvcResult, "$.visibility")),
+                () -> assertTrue(testMembersId.contains(member.getId())),
+                () -> assertEquals(1, testMembersId.size())
+        );
+    }
 
-                () -> assertEquals(HttpStatus.CREATED.value(), secondMvcResult.getResponse().getStatus()),
-                () -> assertNotNull(getValue(secondMvcResult, "$.id")),
-                () -> assertEquals(secondWorkspace.getCreatedBy(), getValue(secondMvcResult, "$.createdBy")),
-                () -> assertEquals(String.valueOf(LocalDate.now()), getValue(secondMvcResult, "$.createdDate")),
-                () -> assertNull(getValue(secondMvcResult, "$.updatedBy")),
-                () -> assertNull(getValue(secondMvcResult, "$.updatedDate")),
-                () -> assertEquals(secondWorkspace.getName(), getValue(secondMvcResult, "$.name")),
-                () -> assertEquals(secondWorkspace.getDescription(),getValue(secondMvcResult, "$.description")),
-                () -> assertEquals(secondWorkspace.getVisibility().toString(), getValue(secondMvcResult, "$.visibility")),
-                () -> assertTrue(testSecondMembersId.contains(firstMember.getId())),
-                () -> assertTrue(testSecondMembersId.contains(secondMember.getId())),
-                () -> assertEquals(2, testSecondMembersId.size())
+    @Test
+    public void createFromArg() throws Exception {
+        Member firstMember = helper.getNewMember("1create@WIT");
+        Member secondMember = helper.getNewMember("2create@WIT");
+        Workspace workspace = new Workspace();
+        workspace.setCreatedBy(firstMember.getCreatedBy());
+        Set<UUID> membersId = new HashSet<>();
+        membersId.add(firstMember.getId());
+        membersId.add(secondMember.getId());
+        workspace.setMembersId(membersId);
+        workspace.setName("name");
+        workspace.setDescription("description");
+        workspace.setVisibility(WorkspaceVisibility.PUBLIC);
+        MvcResult mvcResult = super.create(URL_TEMPLATE, workspace);
+        Set<UUID> testMembersId = helper.getIdsFromJson(getValue(mvcResult, "$.membersId").toString());
+
+        assertAll(
+                () -> assertEquals(HttpStatus.CREATED.value(), mvcResult.getResponse().getStatus()),
+                () -> assertNotNull(getValue(mvcResult, "$.id")),
+                () -> assertEquals(workspace.getCreatedBy(), getValue(mvcResult, "$.createdBy")),
+                () -> assertEquals(String.valueOf(LocalDate.now()), getValue(mvcResult, "$.createdDate")),
+                () -> assertNull(getValue(mvcResult, "$.updatedBy")),
+                () -> assertNull(getValue(mvcResult, "$.updatedDate")),
+                () -> assertEquals(workspace.getName(), getValue(mvcResult, "$.name")),
+                () -> assertEquals(workspace.getDescription(), getValue(mvcResult, "$.description")),
+                () -> assertEquals(workspace.getVisibility().toString(), getValue(mvcResult, "$.visibility")),
+                () -> assertTrue(testMembersId.contains(firstMember.getId())),
+                () -> assertTrue(testMembersId.contains(secondMember.getId())),
+                () -> assertEquals(2, testMembersId.size())
         );
     }
 
     @Test
     public void createFailure() throws Exception {
-        User entity = new User();
+        Workspace entity = new Workspace();
         MvcResult mvcResult = super.create(URL_TEMPLATE, entity);
 
         assertEquals(HttpStatus.BAD_REQUEST.value(), mvcResult.getResponse().getStatus());
@@ -133,11 +141,11 @@ public class WorkspaceIntegrationTest extends AbstractIntegrationTest<User>{
         Workspace workspace = helper.getNewWorkspace("deleteById@WIT");
         MvcResult mvcResult = super.deleteById(URL_TEMPLATE, workspace.getId());
         MvcResult deleteMvcResult = super.findAll(URL_TEMPLATE);
-        List<Workspace> testUsers = helper.getWorkspacesArray(deleteMvcResult);
+        List<Workspace> testWorkspaces = helper.getWorkspacesArray(deleteMvcResult);
 
         assertAll(
                 () -> assertEquals(HttpStatus.OK.value(), mvcResult.getResponse().getStatus()),
-                () -> assertFalse(testUsers.contains(workspace))
+                () -> assertFalse(testWorkspaces.contains(workspace))
         );
     }
 
@@ -152,8 +160,8 @@ public class WorkspaceIntegrationTest extends AbstractIntegrationTest<User>{
 
     @Test
     public void update() throws Exception {
-        Workspace workspace = helper.getNewWorkspace("1update@WIT");
-        Member secondMember = helper.getNewMember("2update@WIT");
+        Workspace workspace = helper.getNewWorkspace("1update@WorkspaceIntegrationTest");
+        Member secondMember = helper.getNewMember("2update@WorkspaceIntegrationTest");
         workspace.setUpdatedBy(workspace.getCreatedBy());
         workspace.setName("new name");
         workspace.setDescription("new description");
@@ -176,6 +184,24 @@ public class WorkspaceIntegrationTest extends AbstractIntegrationTest<User>{
                 () -> assertTrue(testMembersId.contains(workspace.getMembersId().iterator().next())),
                 () -> assertTrue(testMembersId.contains(workspace.getMembersId().iterator().next())),
                 () -> assertEquals(2, membersId.size())
+        );
+    }
+
+    @Test
+    public void updateFailure() throws Exception {
+        Workspace firstWorkspace = helper.getNewWorkspace("1updateFailure@WorkspaceIntegrationTest");
+        firstWorkspace.setName(null);
+        firstWorkspace.setUpdatedBy(firstWorkspace.getCreatedBy());
+
+        Workspace secondWorkspace = new Workspace();
+        secondWorkspace.setId(firstWorkspace.getId());
+
+        MvcResult firstMvcResult = super.update(URL_TEMPLATE, firstWorkspace.getId(), firstWorkspace);
+        MvcResult secondMvcResult = super.update(URL_TEMPLATE, secondWorkspace.getId(), secondWorkspace);
+
+        assertAll(
+                () -> assertEquals(HttpStatus.NOT_FOUND.value(), firstMvcResult.getResponse().getStatus()),
+                () -> assertEquals(HttpStatus.BAD_REQUEST.value(), secondMvcResult.getResponse().getStatus())
         );
     }
 }
