0663604c2dc598e85048b993689c5f47b8e0a278
samas <sama.svm@gmail.com>
2022-04-15 19:26:24 +0300

    added validations for Color

--- src/main/java/spd/trello/domain/Color.java
+++ src/main/java/spd/trello/domain/Color.java
@@ -7,6 +7,7 @@ import spd.trello.domain.perent.Domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.validation.constraints.Size;
 import java.util.UUID;
 
 @Data
@@ -15,11 +16,14 @@ import java.util.UUID;
 @Table(name = "colors")
 public class Color extends Domain {
     @Column(name = "red")
+    @Size(min = 0, max = 255, message = "The red color should be in the range 0 to 255.")
     private Integer red = 0;
 
     @Column(name = "green")
+    @Size(min = 0, max = 255, message = "The green color should be in the range 0 to 255.")
     private Integer green = 0;
 
     @Column(name = "blue")
+    @Size(min = 0, max = 255, message = "The blue color should be in the range 0 to 255.")
     private Integer blue = 0;
 }
