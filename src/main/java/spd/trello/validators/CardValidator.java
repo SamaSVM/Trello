25ceb51c20e36f232ad6fc34da2e64fdde9fd4ac
samas <sama.svm@gmail.com>
2022-04-17 11:53:45 +0300

    Moved repeating logic to helper. All validation exceptions are displayed at once. CardValidator

--- src/main/java/spd/trello/validators/CardValidator.java
+++ src/main/java/spd/trello/validators/CardValidator.java
@@ -5,72 +5,51 @@ import spd.trello.domain.Card;
 import spd.trello.exeption.BadRequestException;
 import spd.trello.repository.CardListRepository;
 import spd.trello.repository.CardRepository;
-import spd.trello.repository.MemberRepository;
-
-import java.util.Set;
-import java.util.UUID;
 
 @Component
 public class CardValidator extends AbstractValidator<Card> {
     private final CardListRepository cardListRepository;
-    private final MemberRepository memberRepository;
     private final ReminderValidator reminderValidator;
     private final CardRepository cardRepository;
+    private final HelperValidator<Card> helper;
 
-    public CardValidator(CardListRepository cardListRepository, MemberRepository memberRepository,
-                         ReminderValidator reminderValidator, CardRepository cardRepository) {
+    public CardValidator(CardListRepository cardListRepository, ReminderValidator reminderValidator,
+                         CardRepository cardRepository, HelperValidator<Card> helper) {
         this.cardListRepository = cardListRepository;
-        this.memberRepository = memberRepository;
         this.reminderValidator = reminderValidator;
         this.cardRepository = cardRepository;
+        this.helper = helper;
     }
 
     @Override
     public void validateSaveEntity(Card entity) {
-
         if (entity.getArchived()) {
             throw new BadRequestException("You cannot create an archived card.");
         }
         reminderValidator.validateSaveEntity(entity.getReminder());
-
+        StringBuilder exceptions = helper.checkCreateEntity(entity);
         if (!cardListRepository.existsById(entity.getCardListId())) {
-            throw new BadRequestException("The cardListId field must belong to a CardList.");
+            exceptions.append("The cardListId field must belong to a CardList.");
         }
-        validMembersId(entity.getMembersId());
-        super.validateSaveEntity(entity);
+        helper.validMembersId(exceptions, entity.getMembersId());
+        helper.throwException(exceptions);
     }
 
     @Override
     public void validateUpdateEntity(Card entity) {
-        Card oldCard = cardRepository.getById(entity.getId());
-        if (!oldCard.getArchived() && !entity.getArchived()) {
+        var oldCard = cardRepository.findById(entity.getId());
+        if (oldCard.isEmpty()) {
+            throw new BadRequestException("Cannot update non-existent card!");
+        }
+        if (!oldCard.get().getArchived() && !entity.getArchived()) {
             throw new BadRequestException("Archived CardList cannot be updated.");
         }
-        if (!oldCard.getCreatedBy().equals(entity.getCreatedBy())) {
-            throw new BadRequestException("The createdBy field cannot be updated.");
-        }
-        if (!oldCard.getCreatedDate().equals(entity.getCreatedDate())) {
-            throw new BadRequestException("The createdDate field cannot be updated.");
-        }
-        if (!oldCard.getCardListId().equals(entity.getCardListId())) {
-            throw new BadRequestException("Card cannot be transferred to another CardList.");
-        }
-        if (entity.getUpdatedBy() == null) {
-            throw new BadRequestException("The updatedBy field must be filled.");
-        }
-        if (entity.getUpdatedDate() == null) {
-            throw new BadRequestException("The updatedDate field must be filled.");
-        }
-        validMembersId(entity.getMembersId());
         reminderValidator.validateUpdateEntity(entity.getReminder());
-        super.validateUpdateEntity(entity);
-    }
-
-    private void validMembersId(Set<UUID> membersId) {
-        for (UUID id : membersId) {
-            if (memberRepository.existsById(id)) {
-                throw new BadRequestException(id + " - memberId must belong to the member.");
-            }
+        StringBuilder exceptions = helper.checkUpdateEntity(oldCard.get(), entity);
+        if (!oldCard.get().getCardListId().equals(entity.getCardListId())) {
+            exceptions.append("Card cannot be transferred to another CardList.");
         }
+        helper.validMembersId(exceptions, entity.getMembersId());
+        helper.throwException(exceptions);
     }
 }
