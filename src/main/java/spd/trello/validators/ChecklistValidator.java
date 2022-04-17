26458f7eefb34d776c65fe994f2a8d5d3f70d186
samas <sama.svm@gmail.com>
2022-04-17 12:24:24 +0300

    Moved repeating logic to helper. All validation exceptions are displayed at once. ChecklistValidator

--- src/main/java/spd/trello/validators/ChecklistValidator.java
+++ src/main/java/spd/trello/validators/ChecklistValidator.java
@@ -10,38 +10,34 @@ import spd.trello.repository.ChecklistRepository;
 public class ChecklistValidator extends AbstractValidator<Checklist> {
     private final CardRepository cardRepository;
     private final ChecklistRepository checklistRepository;
+    private final HelperValidator<Checklist> helper;
 
-    public ChecklistValidator(CardRepository cardRepository, ChecklistRepository checklistRepository) {
+    public ChecklistValidator(CardRepository cardRepository, ChecklistRepository checklistRepository,
+                              HelperValidator<Checklist> helper) {
         this.cardRepository = cardRepository;
         this.checklistRepository = checklistRepository;
+        this.helper = helper;
     }
 
     @Override
     public void validateSaveEntity(Checklist entity) {
+        StringBuilder exceptions = helper.checkCreateEntity(entity);
         if (!cardRepository.existsById(entity.getCardId())) {
-            throw new BadRequestException("The cardId field must belong to a card.");
+            exceptions.append("The cardId field must belong to a card.");
         }
-        super.validateSaveEntity(entity);
+        helper.throwException(exceptions);
     }
 
     @Override
     public void validateUpdateEntity(Checklist entity) {
-        Checklist oldCheckList = checklistRepository.getById(entity.getId());
-        if (!oldCheckList.getCreatedBy().equals(entity.getCreatedBy())) {
-            throw new BadRequestException("The createdBy field cannot be updated.");
+        var oldCheckList = checklistRepository.findById(entity.getId());
+        if (oldCheckList.isEmpty()) {
+            throw new BadRequestException("Cannot update non-existent checklist!");
         }
-        if (!oldCheckList.getCreatedDate().equals(entity.getCreatedDate())) {
-            throw new BadRequestException("The createdDate field cannot be updated.");
+        StringBuilder exceptions = helper.checkUpdateEntity(oldCheckList.get(), entity);
+        if (!oldCheckList.get().getCardId().equals(entity.getCardId())) {
+            exceptions.append("CheckList cannot be transferred to another card. \n");
         }
-        if (!oldCheckList.getCardId().equals(entity.getCardId())) {
-            throw new BadRequestException("CheckList cannot be transferred to another card.");
-        }
-        if (entity.getUpdatedBy() == null) {
-            throw new BadRequestException("The updatedBy field must be filled.");
-        }
-        if (entity.getUpdatedDate() == null) {
-            throw new BadRequestException("The updatedDate field must be filled.");
-        }
-        super.validateUpdateEntity(entity);
+        helper.throwException(exceptions);
     }
 }
