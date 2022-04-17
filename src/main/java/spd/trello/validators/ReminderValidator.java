7e963c5f018401c161eb695cc791b6136e0c88be
samas <sama.svm@gmail.com>
2022-04-17 11:36:38 +0300

    Moved repeating logic to helper. All validation exceptions are displayed at once. ReminderValidator

--- src/main/java/spd/trello/validators/ReminderValidator.java
+++ src/main/java/spd/trello/validators/ReminderValidator.java
@@ -9,45 +9,40 @@ import spd.trello.repository.ReminderRepository;
 public class ReminderValidator extends AbstractValidator<Reminder> {
 
     private final ReminderRepository repository;
+    private final HelperValidator<Reminder> helper;
 
-    public ReminderValidator(ReminderRepository repository) {
+    public ReminderValidator(ReminderRepository repository, HelperValidator<Reminder> helper) {
         this.repository = repository;
+        this.helper = helper;
     }
 
     @Override
     public void validateSaveEntity(Reminder entity) {
-        checkTime(entity);
-        super.validateSaveEntity(entity);
+        StringBuilder exceptions = helper.checkCreateEntity(entity);
+        checkTime(exceptions, entity);
+        helper.throwException(exceptions);
     }
 
     @Override
     public void validateUpdateEntity(Reminder entity) {
-        Reminder oldReminder = repository.getById(entity.getId());
-        if (!oldReminder.getActive() && !entity.getActive()) {
-            throw new BadRequestException("Cannot update an inactive remainder.");
+        var oldReminder = repository.findById(entity.getId());
+        if (oldReminder.isEmpty()) {
+            throw new BadRequestException("Cannot update non-existent workspace!");
         }
-        if (!oldReminder.getCreatedBy().equals(entity.getCreatedBy())) {
-            throw new BadRequestException("The createdBy field cannot be updated.");
+        StringBuilder exceptions = helper.checkUpdateEntity(oldReminder.get(), entity);
+        if (!oldReminder.get().getActive() && !entity.getActive()) {
+            exceptions.append("Cannot update an inactive remainder.");
         }
-        if (!oldReminder.getCreatedDate().equals(entity.getCreatedDate())) {
-            throw new BadRequestException("The createdDate field cannot be updated.");
-        }
-        if (entity.getUpdatedBy() == null) {
-            throw new BadRequestException("The updatedBy field must be filled.");
-        }
-        if (entity.getUpdatedDate() == null) {
-            throw new BadRequestException("The updatedDate field must be filled.");
-        }
-        checkTime(entity);
-        super.validateUpdateEntity(entity);
+        checkTime(exceptions, entity);
+        helper.throwException(exceptions);
     }
 
-    public void checkTime(Reminder entity) {
+    public void checkTime(StringBuilder exceptions, Reminder entity) {
         if (entity.getStart().isBefore(entity.getEnd())) {
-            throw new BadRequestException("Start cannot be later than end.");
+            exceptions.append("Start cannot be later than end.");
         }
         if (entity.getStart().isAfter(entity.getRemindOn()) || entity.getEnd().isBefore(entity.getRemindOn())) {
-            throw new BadRequestException("The remindOn should be between start and end.");
+            exceptions.append("The remindOn should be between start and end.");
         }
     }
 }
