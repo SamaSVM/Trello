5aa84dc13818063722a4ca47ac38f44e02c29e34
samas <sama.svm@gmail.com>
2022-04-17 11:03:40 +0300

    Moved repeating logic to helper. All validation exceptions are displayed at once. BoardValidator

--- src/main/java/spd/trello/validators/BoardValidator.java
+++ src/main/java/spd/trello/validators/BoardValidator.java
@@ -4,21 +4,16 @@ import org.springframework.stereotype.Component;
 import spd.trello.domain.Board;
 import spd.trello.exeption.BadRequestException;
 import spd.trello.repository.BoardRepository;
-import spd.trello.repository.MemberRepository;
-
-import java.util.Set;
-import java.util.UUID;
 
 @Component
-public class BoardValidator extends AbstractValidator<Board>{
-
-    private final MemberRepository memberRepository;
+public class BoardValidator extends AbstractValidator<Board> {
 
     private final BoardRepository boardRepository;
+    private final HelperValidator<Board> helper;
 
-    public BoardValidator(MemberRepository memberRepository, BoardRepository boardRepository) {
-        this.memberRepository = memberRepository;
+    public BoardValidator(BoardRepository boardRepository, HelperValidator<Board> helper) {
         this.boardRepository = boardRepository;
+        this.helper = helper;
     }
 
     @Override
@@ -26,40 +21,25 @@ public class BoardValidator extends AbstractValidator<Board>{
         if (entity.getArchived()) {
             throw new BadRequestException("You cannot create an archived board.");
         }
-        validMembersId(entity.getMembersId());
-        super.validateSaveEntity(entity);
+        StringBuilder exceptions = helper.checkCreateEntity(entity);
+        helper.validMembersId(exceptions, entity.getMembersId());
+        helper.throwException(exceptions);
     }
 
     @Override
     public void validateUpdateEntity(Board entity) {
-        Board oldBoard = boardRepository.getById(entity.getId());
-        if(!oldBoard.getArchived() && !entity.getArchived()){
+        var oldBoard = boardRepository.findById(entity.getId());
+        if (oldBoard.isEmpty()) {
+            throw new BadRequestException("Cannot update non-existent workspace!");
+        }
+        if (!oldBoard.get().getArchived() && !entity.getArchived()) {
             throw new BadRequestException("Archived board cannot be updated.");
         }
-        if (!oldBoard.getWorkspaceId().equals(entity.getWorkspaceId())) {
-            throw new BadRequestException("Board cannot be transferred to another workspace.");
+        StringBuilder exceptions = helper.checkUpdateEntity(oldBoard.get(), entity);
+        if (!oldBoard.get().getWorkspaceId().equals(entity.getWorkspaceId())) {
+            exceptions.append("Board cannot be transferred to another workspace.");
         }
-        if(!oldBoard.getCreatedBy().equals(entity.getCreatedBy())){
-            throw new BadRequestException("The createdBy field cannot be updated.");
-        }
-        if(!oldBoard.getCreatedDate().equals(entity.getCreatedDate())){
-            throw new BadRequestException("The createdDate field cannot be updated.");
-        }
-        if (entity.getUpdatedBy() == null) {
-            throw new BadRequestException("The updatedBy field must be filled.");
-        }
-        if (entity.getUpdatedDate() == null) {
-            throw new BadRequestException("The updatedDate field must be filled.");
-        }
-        validMembersId(entity.getMembersId());
-        super.validateUpdateEntity(entity);
-    }
-
-    private void validMembersId(Set<UUID> membersId) {
-        for (UUID id: membersId) {
-            if (memberRepository.existsById(id)) {
-                throw new BadRequestException(id + " - memberId must belong to the member.");
-            }
-        }
+        helper.validMembersId(exceptions, entity.getMembersId());
+        helper.throwException(exceptions);
     }
 }
